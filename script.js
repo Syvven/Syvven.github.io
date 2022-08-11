4cc99ba8d729f5532dd2aa4f098e12b5f88485bd
Noah Hendrickson <nhendrickson2001@gmail.com>
2022-08-11 17:22:20 -0500

    moooooooore format

--- script.js
+++ script.js
@@ -30,14 +30,16 @@ for (let i = 1; i <= num_images; i++) {
     sounds.push(sound);
 }
 
+console.log(canvas.width);
+
 let ravens = [];
 class Raven {
     constructor() {
         this.spriteWidth = 687;
         this.spriteHeight = 786;
         this.sizeModifier = Math.random() * 0.6 + 0.6;
-        this.width = (this.spriteWidth * 0.3 * this.sizeModifier) / (canvas.width * 0.00075 * 0.7);
-        this.height = (this.spriteHeight * 0.3 * this.sizeModifier) / (canvas.height * 0.001 * 0.7);
+        this.width = (this.spriteWidth * 0.3 * this.sizeModifier) * (canvas.width * 0.00075 * 0.7);
+        this.height = (this.spriteHeight * 0.3 * this.sizeModifier) * (canvas.height * 0.001 * 0.7);
         this.x = canvas.width;
         this.y = Math.random() * (canvas.height - this.height);
         this.directionXbase = Math.random() * 5 + 3;
